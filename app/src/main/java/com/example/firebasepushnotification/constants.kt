946feef542911ba58eda185b8a16b4895604ee51
prateekpandey1234 <76869230+prateekpandey1234@users.noreply.github.com>
2022-01-02 11:31:27 +0530

    Update constants.kt

--- app/src/main/java/com/example/firebasepushnotification/constants.kt
+++ app/src/main/java/com/example/firebasepushnotification/constants.kt
@@ -3,7 +3,7 @@ package com.example.firebasepushnotification
 class constants {
     companion object{
         const val Base_url= "https://fcm.googleapis.com"
-        const val Server_key="AAAAK5N0NTQ:APA91bFntKdvVmxLo7YGhU7sM_tjv9PGYrBSwZ9dGpxQF7Q82sziyttPqtPbJalk3N9piOW6TQ7oNuKE1xdW8aXzXGoeUZF_HGF0bDcbM8TnbBaYiLUpO8guGTnGEPeKEtv83ooZcE8H"
+        const val Server_key="my server key"
         const val Content_type="application/json"//content type request we want to make....only use this
     }
 }
